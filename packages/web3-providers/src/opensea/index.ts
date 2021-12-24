b1542c40ff2720be0c3ff178cdb008be94c70ba8
Lantt <laanfor@mask.io>
2021-12-24 12:12:06 +0800

    feat: add fetch non fungible collectible from opensea

--- packages/web3-providers/src/opensea/index.ts
+++ packages/web3-providers/src/opensea/index.ts
@@ -1,8 +1,9 @@
 import type { ChainId } from '@masknet/web3-shared-evm'
-import type { AssetsListResponse } from './type'
+import type { AssetsListResponse, AssetCollection } from './type'
 import { format } from './format'
 
 export const OPENSEA_API_KEY = 'c38fe2446ee34f919436c32db480a2e3'
+export const OPENSEA_API = 'https://api.opensea.io'
 
 export async function getAssetsListFromOpenSea(
     from: string,
@@ -18,7 +19,7 @@ export async function getAssetsListFromOpenSea(
         params.append('collection', collection)
     }
 
-    const response = await fetch(`https://api.opensea.io/api/v1/assets?${params.toString()}`, {
+    const response = await fetch(`${OPENSEA_API}/api/v1/assets?${params.toString()}`, {
         method: 'GET',
         mode: 'cors',
         headers: {
@@ -26,6 +27,25 @@ export async function getAssetsListFromOpenSea(
         },
     })
     return (await response.json()) as AssetsListResponse
+}
+
+export async function getCollectionsFromOpenSea(owner: string, opts: { page?: number; size?: number }) {
+    const { page = 0, size = 300 } = opts
+    const params = new URLSearchParams()
+    params.append('asset_owner', owner.toLowerCase())
+    params.append('limit', String(size))
+    params.append('offset', String(size * page))
+
+    const response = await fetch(`${OPENSEA_API}/api/v1/collections?${params.toString()}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'x-api-key': OPENSEA_API_KEY,
+        },
+    })
+    const collections = (await response.json()) as AssetCollection[]
+
+    return { collections }
 }
 
 export async function getOpenSeaNFTList(
@@ -37,4 +57,21 @@ export async function getOpenSeaNFTList(
 ) {
     const { assets } = await getAssetsListFromOpenSea(address, { page, size, collection }, apiKey)
     return format(address, size ?? 50, assets)
+}
+
+export interface Collection {
+    name: string
+    image: string
+    slug: string
+}
+export async function getOpenSeaCollectionList(apiKey: string, address: string, page?: number, size?: number) {
+    const { collections } = await getCollectionsFromOpenSea(address, { page, size })
+    return {
+        data: collections.map((x) => ({
+            name: x.name,
+            image: x.image_url || undefined,
+            slug: x.slug,
+        })) as Collection[],
+        hasNextPage: collections.length === size,
+    }
 }
